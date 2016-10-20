5b8e429ae1d6c7f6a6f5a0f2b8230e77f8c329cd
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2016-10-20 23:28:53 +0900

    使い方ページのキャッシュを一時的に無効化

--- app/controllers/manual_controller.rb
+++ app/controllers/manual_controller.rb
@@ -2,8 +2,11 @@ class ManualController < ApplicationController
   def exporter
     set_meta_tags title: 'Admiral Stats の使い方'
 
+    # メニューバーまでキャッシュされてしまう（キャッシュ時のログイン状態のまま表示されてしまう）ため
+    # キャッシュを無効化した。あとで、本文だけキャッシュするように直す。
+
     # 静的コンテンツのため、有効期限を設定
-    expires_in 1.hour
+    # expires_in 1.hour
 
     # ブラウザにキャッシュさせたくない場合は以下のようにする
     # expires_now
