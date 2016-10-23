e95660837ce945d1b459222cd950e75caa2e5120
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2016-10-23 18:01:18 +0900

    限定海域のマスターデータを登録するテーブルの追加（テーブル名は仮の名前。最終的には、イベント開始後にプレイヤーズサイトで使われる単語を採用する）

---
+++ app/models/campaign_master.rb
@@ -0,0 +1,26 @@
+class CampaignMaster < ApplicationRecord
+  self.primary_key = 'campaign_no'
+
+  # 期間限定海域 No. （例：第壱回なら 1）
+  validates :campaign_no,
+            presence: true,
+            uniqueness: true,
+            numericality: {
+                only_integer: true,
+                greater_than_or_equal_to: 1,
+            }
+
+
+  # 期間限定海域名
+  validates :campaign_name,
+            presence: true,
+            length: { minimum: 1, maximum: 32 }
+
+  # 開始時刻
+  validates :started_at,
+            presence: true
+
+  # 終了時刻
+  validates :ended_at,
+            presence: true
+end
---
+++ test/models/campaign_master_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CampaignMasterTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
