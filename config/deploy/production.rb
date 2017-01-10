8302374fa9a4515acfd3ddba76eb102af06f18dd
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2017-01-10 22:22:57 +0900

    ホスト名を変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # You can define all roles on a single server, or split them:
 
 # db:migrate を自動実行しないようにするため、roles から db は外した
-server 'as-web01', user: 'admiral', roles: %w{app web}
+server 'as-web1', user: 'admiral', roles: %w{app web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
