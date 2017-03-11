394971937a4dc414cf01c61cdf2444c5876c9ac0
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2017-03-11 13:38:25 +0900

    CORS 設定の置き場所の変更

---
+++ config/initializers/cors.rb
@@ -0,0 +1,15 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # Insert CORS request headers for API responses
+  allow do
+    # Access-Control-Allow-Origin: (Origin に書かれたものをそのまま返す)
+    origins '*'
+
+    # Access-Control-Allow-Methods: GET, POST, OPTIONS
+    # Access-Control-Allow-Headers: (OPTIONS に対してのみ Access-Control-Request-Headers に書かれたものをそのまま返す)
+    # Access-Control-Max-Age: 3600
+    resource '/api/*',
+             :methods => [:get, :post, :options],
+             :headers => :any,
+             :max_age => 3600
+  end
+end
