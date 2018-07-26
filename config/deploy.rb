3b855c7745a07b41e11d4a8c76506992a133eb3b
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2018-07-26 23:46:40 +0900

    Update Cap version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.10.1'
+lock '3.11.0'
 
 set :application, 'admiral_stats'
 set :repo_url, 'git@github.com:muziyoshiz/admiral_stats.git'
