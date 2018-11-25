896897cb180f0ee7facb2c41e3e11592dad634d1
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2018-11-25 15:12:07 +0900

    イベント攻略率の集計バッチの追加

---
+++ app/batches/event_stats_export_batch.rb
@@ -0,0 +1,99 @@
+# イベント攻略率を時系列に集計し、出力するバッチです。
+# 実行方法は以下の通りです。-e オプションを指定しない場合は、development で実行されます。
+# rails runner EventStatsExportBatch.execute -e (development|production)
+class EventStatsExportBatch
+  def self.execute
+    # エラーチェックは省略
+    event_no = ARGV[0].to_i
+
+    event = EventMaster.find_by_event_no(event_no)
+    unless event
+      print "ERROR: Invalid event number '#{ARGV[0]}'\n"
+      return
+    end
+
+    # TSV ヘッダの出力
+    row = []
+    row << '日付'
+    event.periods.each do |period|
+      s_period = case period
+                   when 0
+                     '前段作戦'
+                   when 1
+                     '後段作戦'
+                   when 2
+                     'EO'
+                 end
+
+      event.levels_in_period(period).each do |level|
+        s_level = case level
+                    when 'HEI'
+                      '丙'
+                    when 'OTU'
+                      '乙'
+                    when 'KOU'
+                      '甲'
+                  end
+
+        row << "#{s_period} #{s_level} 提督数"
+        row << "#{s_period} #{s_level} 攻略済み提督数"
+        row << "#{s_period} #{s_level} 攻略率"
+        row << "#{s_period} #{s_level} 攻略済み周回数（合計）"
+        row << "#{s_period} #{s_level} 攻略済み周回数（平均）"
+        row << "#{s_period} #{s_level} 攻略済み周回数（最大）"
+      end
+    end
+
+    print row.join("\t")
+    print "\n"
+
+    # ループの開始日
+    day = event.started_at.end_of_day
+    # ループの終了日（イベント終了1週間後まではプレイデータをエクスポート可能）
+    end_of_range = event.ended_at.end_of_day + 7.day
+
+    while day <= end_of_range do
+      # TSV 1行分のデータ
+      row = []
+      row << day.strftime('%Y-%m-%d')
+
+      # 各提督の、攻略済み周回数を、作戦および難易度別に取得
+      event.periods.each do |period|
+        event.levels_in_period(period).each do |level|
+          statuses = EventProgressStatus.find_by_sql(
+              [ 'SELECT admiral_id, max(cleared_loop_counts) AS max_cleared_loop_counts ' +
+                    'FROM event_progress_statuses WHERE event_no = ? AND period = ? AND level = ? AND exported_at <= ?' +
+                    'GROUP BY admiral_id',
+                event_no, period, level, day ]
+          ).to_a
+
+          # その作戦および難易度のプレイデータをアップロードした提督数
+          admiral_num = statuses.size
+          row << admiral_num
+
+          # その作戦および難易度をクリアした提督数
+          cleared_admiral_num = statuses.select{|s| s.max_cleared_loop_counts > 0 }.size
+          row << cleared_admiral_num
+
+          # その作戦および難易度をクリアした提督の割合
+          row << (admiral_num == 0 ? 0 : (cleared_admiral_num.to_f / admiral_num * 100).round(1))
+
+          # その作戦および難易度の周回数の合計値
+          loop_sum = statuses.map{|s| s.max_cleared_loop_counts }.inject(:+)
+          row << loop_sum
+
+          # その作戦および難易度の周回数の平均値
+          row << (admiral_num == 0 ? 0 : (loop_sum.to_f / admiral_num).round(1))
+
+          # その作戦および難易度の周回数の最大値
+          row << statuses.map{|s| s.max_cleared_loop_counts }.max
+        end
+      end
+
+      print row.join("\t")
+      print "\n"
+
+      day = day + 1.day
+    end
+  end
+end
